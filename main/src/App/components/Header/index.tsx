e38bd11eba66f4b87c707b2c33418cd3850a174f
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2026-01-12 22:18:05 -0700

    Deploy --- 01/12/2026 10:18:05 PM

--- main/src/App/components/Header/index.tsx
+++ main/src/App/components/Header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => (
                 <h1 className="font-serif text-3xl font-bold text-primary sm:text-4xl md:text-5xl">Heritage Park YSA Ward, 26&ndash;35</h1>
                 <div className="mt-4 flex w-full items-start justify-center">
                     <hr className="flex-grow border-t border-primary" />
-                    <span className="-mt-[0.8125rem] px-4 text-xl uppercase text-primary md:-mt-5 md:px-6 md:text-2xl">Sundays @ 12:30PM</span>
+                    <span className="-mt-[0.8125rem] px-4 text-xl uppercase text-primary md:-mt-5 md:px-6 md:text-2xl">Sundays @ 10AM</span>
                     <hr className="flex-grow border-t border-primary" />
                 </div>
             </section>
