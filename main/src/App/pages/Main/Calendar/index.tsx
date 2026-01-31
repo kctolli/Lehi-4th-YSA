289a2e9eb27a8373563eeb7ce6bea773ebab32c4
kctolli <developer@ktolliver.org>
2026-01-31 01:23:23 -0700

    Deploy --- 01/31/2026 01:23:23 AM

--- main/src/App/pages/Main/Calendar/index.tsx
+++ main/src/App/pages/Main/Calendar/index.tsx
@@ -6,7 +6,7 @@ const Calendar = ({ type }: CalendarType) => {
     const url =
         type === 'stake'
             ? 'https://calendar.google.com/calendar/u/0/embed?height=600&wkst=1&ctz=America%2FDenver&title=Alpine%20Utah%20YSA%20Stake&color=%23B3DC6C&mode=AGENDA&src=223amftqs12rme8eod96al6e8jgdaekt@import.calendar.google.com'
-            : 'https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FDenver&title=Heritage%20Park%20YSA%20Ward%2C%2026-35&src=aHB5c2F3ZWJAZ21haWwuY29t&color=%23039BE5&mode=AGENDA';
+            : 'https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FDenver&title=Heritage%20Park%20YSA%20Ward%2C%2026-35&src=NGU5NjJlZDkyOTg3YTQzZjE2NTVkZjdmNzg1MTA3OTFhMDBjOTFjODNmMTM2NTUxNjIzMTlmYzA3NmYwY2Y5MEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&mode=AGENDA';
 
     return <iframe className="w-full rounded border shadow-md" src={url} width="800" height="600"></iframe>;
 };
