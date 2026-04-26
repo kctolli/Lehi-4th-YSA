f7ae2018fe0f57cc9455a9ba1b95cf57f23617c9
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2026-04-26 10:39:26 -0600

    Update index.tsx

--- main/src/App/components/Header/index.tsx
+++ main/src/App/components/Header/index.tsx
@@ -2,16 +2,11 @@ import { Card } from 'antd';
 import Banner from './Banner';
 
 import ChurchDetails from './ChurchDetails';
-
-const BishopInfo = {
-    name: 'Bishop Mugleston',
-    phone: '(801) 367-0602',
-};
-
-const ExecutiveSecretaryInfo = {
-    name: 'Executive Secretary',
-    phone: '(209) 790-9348',
-};
+import {
+    BishopInfo, 
+    ExecutiveSecretaryInfo, 
+    ActivityInfo
+} from './lib';
 
 const Header = () => (
     <>
@@ -30,22 +25,30 @@ const Header = () => (
                 </Card>
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium sm:text-lg">EXECUTIVE SECRETARY</span>
+                        <span className="font-medium sm:text-lg">{ExecutiveSecretaryInfo.name}</span>
                         <a className="text-tertiary transition-all hover:text-tertiary hover:underline" href={`tel: ${ExecutiveSecretaryInfo.phone}`} aria-label="Executive Secretary's Phone Number">
                             {ExecutiveSecretaryInfo.phone}
                         </a>
                     </div>
                 </Card>
+            </section>
+            <section className="text-md mx-auto grid w-full grid-cols-1 grid-rows-3 gap-6 sm:grid-cols-[repeat(2,minmax(15rem,1fr))] lg:gap-8">
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium sm:text-lg">ACTIVITY NIGHT</span>
-                        <span>Mondays @ 7:00PM</span>
+                        <span className="font-medium sm:text-lg">Family Home Evening</span>
+                        <span>{ActivityInfo.fhe}</span>
                     </div>
                 </Card>
                 <Card className="border-0">
                     <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
-                        <span className="font-medium sm:text-lg">STAKE NIGHT</span>
-                        <span>Thursdays @ 6:30PM</span>
+                        <span className="font-medium sm:text-lg">Game Night</span>
+                        <span>{ActivityInfo.game}</span>
+                    </div>
+                </Card>
+                <Card className="border-0">
+                    <div className="flex flex-col items-center justify-center gap-1 rounded border p-4 font-sans text-base uppercase text-primary shadow-md sm:p-4">
+                        <span className="font-medium sm:text-lg">Stake Night</span>
+                        <span>{ActivityInfo.stake}</span>
                     </div>
                 </Card>
             </section>
