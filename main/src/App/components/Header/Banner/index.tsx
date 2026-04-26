c6148cabbc5176574b01fe169f19ea41e04fe33d
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2026-04-26 10:44:57 -0600

    Update index.tsx

--- main/src/App/components/Header/Banner/index.tsx
+++ main/src/App/components/Header/Banner/index.tsx
@@ -12,9 +12,7 @@ const Banner = () => (
                 delay: 4000,
                 pauseOnMouseEnter: true
             }}
-            pagination={{
-                clickable: true
-            }}
+            pagination={{clickable: true}}
             autoHeight={true}
             direction={'vertical'}
             loop={true}
@@ -48,6 +46,14 @@ const Banner = () => (
                     <a className="flex items-center justify-center gap-3 px-4 py-2 font-serif font-bold uppercase text-primary transition-all hover:text-tertiary hover:underline xxs:gap-4 sm:text-lg" href="https://www.churchofjesuschrist.org/study" aria-label="Gospel Library" target="_blank">
                         <img className="size-16 sm:size-20" src="https://res.cloudinary.com/hpysa/f_auto,q_auto/alpine-utah-ysa-stake" alt="Alpine Utah YSA Stake" />
                         <span>GOSPEL LIBRARY</span>
+                    </a>
+                </Card>
+            </SwiperSlide>
+            <SwiperSlide className="!h-fit">
+                <Card className="border-0 bg-white text-center [&>div]:!p-0">
+                    <a className="flex items-center justify-center gap-3 px-4 py-2 font-serif font-bold uppercase text-primary transition-all hover:text-tertiary hover:underline xxs:gap-4 sm:text-lg" href="https://kctolli-come-follow-me.vercel.app/" aria-label="Come Follow Me" target="_blank">
+                        <img className="size-16 sm:size-20" src="https://res.cloudinary.com/hpysa/f_auto,q_auto/alpine-utah-ysa-stake" alt="Alpine Utah YSA Stake" />
+                        <span>COME FOLLOW ME</span>
                     </a>
                 </Card>
             </SwiperSlide>
