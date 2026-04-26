04b446532252dae8b90a81c0473a0d458fc4ad71
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2026-04-26 10:32:46 -0600

    Update lib.ts

--- main/src/App/components/Header/lib.ts
+++ main/src/App/components/Header/lib.ts
@@ -1,1 +1,10 @@
+export const BishopInfo = {
+    name: 'Bishop Mugleston',
+    phone: '(801) 367-0602',
+};
+
+export const ExecutiveSecretaryInfo = {
+    name: 'Executive Secretary',
+    phone: '(209) 790-9348',
+};
 
