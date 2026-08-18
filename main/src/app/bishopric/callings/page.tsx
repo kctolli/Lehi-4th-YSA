ebf4ac9245f4f8a727c8be92ee502f215d21fb7a
kctolli <developer@ktolliver.org>
2026-08-18 14:46:32 -0600

    Deploy --- 08/18/2026 02:46:32 PM

--- main/src/app/bishopric/callings/page.tsx
+++ main/src/app/bishopric/callings/page.tsx
@@ -213,7 +213,7 @@ const CallingsPage = () => {
     };
 
     return (
-        <section className="flex flex-col gap-4">
+        <section className="-ml-4 flex flex-col gap-4 lg:ml-0">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl font-semibold">Callings</h1>
                 <Button type="primary" onClick={openCreateModal}>
