a7e5b157f4096b2626019bb1fc732e7cb4b113cb
kctolli <developer@ktolliver.org>
2026-09-13 11:41:52 -0600

    Deploy --- 09/13/2026 11:41:52 AM

--- main/src/app/admin/page.tsx
+++
@@ -1,3 +0,0 @@
-const AdminIndexPage = () => <p className="text-gray-500">No admin tools are configured.</p>;
-
-export default AdminIndexPage;
--- main/src/app/page.tsx
+++ main/src/app/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 
-const HomePage = () => {
+const HomePage = () => 
     redirect('https://local.churchofjesuschrist.org/u/472050?icid=actsh|sh|copy|mis|global|global');
-};
 
 export default HomePage;
