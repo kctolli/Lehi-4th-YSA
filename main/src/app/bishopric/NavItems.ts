d6fef3a9eef49d83413a28da8055b5a2fa51ea20
kctolli <developer@ktolliver.org>
2026-09-13 20:59:02 -0600

    Deploy --- 09/13/2026 08:59:02 PM

--- main/src/app/bishopric/NavItems.ts
+++ main/src/app/bishopric/NavItems.ts
@@ -1,7 +1,7 @@
 const NAV_ITEMS = [
     { href: '/bishopric/callings', label: 'Callings' },
     { href: '/bishopric/sacrament-attendance', label: 'Sacrament Attendance' },
-    { href: '/bishopric/second-hour-attendance', label: '2nd Hour Attendance' },
+    //{ href: '/bishopric/second-hour-attendance', label: '2nd Hour Attendance' },
     { href: '/bishopric/move-in', label: 'Move-Ins' },
     { href: '/bishopric/video-audit', label: 'Audit Video Tracker' },
 ];
